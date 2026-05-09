82192efa8323cc9e9b78e9399c89071cd3ec619a
paulowuor500 <paulowuor500@gmail.com>
2026-05-09 16:47:52 +0300

    correction on printer.go and html files

--- internal/ascii/printer.go
+++ internal/ascii/printer.go
@@ -6,29 +6,38 @@ import (
 )
 
 func GenerateASCII(input, banner string) (string, error) {
-	filepath := "banners/" + banner + ".txt"
+	filePath := "banners/" + banner + ".txt"
 
-	font, err := loadBanner(filepath)
+	font, err := loadBanner(filePath)
 	if err != nil {
 		return "", err
 	}
 
 	var result strings.Builder
 
+	// Split multiline input
 	lines := strings.Split(input, "\n")
 
 	for _, line := range lines {
-		for i := 0; i < 8; i++ {
+
+		// ASCII characters are 8 rows tall
+		for row := 0; row < 8; row++ {
+
+			// Process each character in the line
 			for _, char := range line {
+
+				// Validate printable ASCII range
 				if char < 32 || char > 126 {
 					return "", errors.New("unsupported character")
 				}
 
 				index := int(char - 32)
 
-				result.WriteString(font[index*9+i])
+				// Append the correct ASCII row
+				result.WriteString(font[index*9+row])
 			}
 
+			// Move to next ASCII row
 			result.WriteString("\n")
 		}
 	}
